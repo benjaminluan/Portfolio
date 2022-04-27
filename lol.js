f1de5593c988100f92cc380af4083852e9bd8803
benjaminluan <benjaminluan1@gmail.com>
2022-04-26 21:23:41 -0400

    WOOO

--- lol.js
+++ lol.js
@@ -1,7 +1,7 @@
 let champion;
 let championValuesIntoArray = renderChampions();
 
-async function renderChampions() {
+async function renderChampions(filter) {
     const champions = await fetch("http://ddragon.leagueoflegends.com/cdn/12.7.1/data/en_US/champion.json")
     const championsData = await champions.json();
     const championValues = Object.values(championsData)
@@ -9,11 +9,26 @@ async function renderChampions() {
     championValuesIntoArray = Object.values(championValueData)
     const championsEl = document.querySelector(".champions")
     championsEl.innerHTML = championValuesIntoArray.map((champion) => championHTML(champion) ).join("")
-    return championValuesIntoArray
+
+
+    if (filter === 'All'){
+        championsEl.innerHTML = championValuesIntoArray.map((champion) => championHTML(champion) ).join("")
+    } else if (filter === "Assassin"){
+        championsEl.innerHTML = championValuesIntoArray.filter((champion) => (champion.tags.includes("Assassin"))).map(champion =>championHTML(champion) ).join("")
+    } else if (filter === "Fighter"){
+        championsEl.innerHTML = championValuesIntoArray.filter((champion) => (champion.tags.includes("Fighter"))).map(champion =>championHTML(champion) ).join("")
+    } else if (filter === "Mage"){
+        championsEl.innerHTML = championValuesIntoArray.filter((champion) => (champion.tags.includes("Mage"))).map(champion =>championHTML(champion) ).join("")
+    } else if (filter === "Marksman"){
+        championsEl.innerHTML = championValuesIntoArray.filter((champion) => (champion.tags.includes("Marksman"))).map(champion =>championHTML(champion) ).join("")
+    } else if (filter === "Tank"){
+        championsEl.innerHTML = championValuesIntoArray.filter((champion) => (champion.tags.includes("Tank"))).map(champion =>championHTML(champion) ).join("")
+    } else if (filter === "Support"){
+        championsEl.innerHTML = championValuesIntoArray.filter((champion) => (champion.tags.includes("Support"))).map(champion =>championHTML(champion) ).join("")
+    }
 }
+
 renderChampions()
-
-
 
 
 function championHTML (champion){
@@ -26,6 +41,11 @@ function championHTML (champion){
     </div>`
 }
 
-function dropdown() {
-    document.getElementById("search__dropdown-list").classList.toggle("show");
+function championFilter(event){
+    renderChampions(event.target.textContent)
 }
+
+function filterDifficulty(event){
+    console.log(event)
+}
+
